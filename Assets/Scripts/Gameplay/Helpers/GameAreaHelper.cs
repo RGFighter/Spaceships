3f370d5eb02895688d652a082b53532d06719ab8
RGFighter <sc3ragefighter@gmail.com>
2021-10-01 05:00:12 +0300

    Доработано передвижение корабля игрока
    1. Доработан класс GameAreaHelper
    2. Реализовано ограничение передвижения корабля игрока.

--- Assets/Scripts/Gameplay/Helpers/GameAreaHelper.cs
+++ Assets/Scripts/Gameplay/Helpers/GameAreaHelper.cs
@@ -4,34 +4,49 @@ namespace Gameplay.Helpers
 {
     public static class GameAreaHelper
     {
-
         private static Camera _camera;
-        
+
+        private static float topBound; // Вынесено в приватное поле.
+        private static float bottomBound; // Вынесено в приватное поле.
+        private static float leftBound; // Вынесено в приватное поле.
+        private static float rightBound; // Вынесено в приватное поле.
 
         static GameAreaHelper()
         {
             _camera = Camera.main;
+            CalculateGameplayArea (); // Вычисление рамок игрового поля при инициализации.
         }
-
-        
-        public static bool IsInGameplayArea(Transform objectTransform, Bounds objectBounds)
-        {
+        // Позволяет вычислить рамки игрового поля. Вынесено в отдельный метод для экономии ресурсов.
+        // Вызывать при наличии фактической необходимости (изменение положения камеры, изменение соотношения сторон и т.д.)
+        public static void CalculateGameplayArea () {
             var camHalfHeight = _camera.orthographicSize;
             var camHalfWidth = camHalfHeight * _camera.aspect;
             var camPos = _camera.transform.position;
-            var topBound = camPos.y + camHalfHeight;
-            var bottomBound = camPos.y - camHalfHeight;
-            var leftBound = camPos.x - camHalfWidth;
-            var rightBound = camPos.x + camHalfWidth;
-
+            topBound = camPos.y + camHalfHeight;
+            bottomBound = camPos.y - camHalfHeight;
+            leftBound = camPos.x - camHalfWidth;
+            rightBound = camPos.x + camHalfWidth;
+        }
+        public static bool IsInGameplayArea(Transform objectTransform, Bounds objectBounds)
+        {
             var objectPos = objectTransform.position;
 
             return (objectPos.x - objectBounds.extents.x < rightBound)
                 && (objectPos.x + objectBounds.extents.x > leftBound)
                 && (objectPos.y - objectBounds.extents.y < topBound)
                 && (objectPos.y + objectBounds.extents.y > bottomBound);
-
         }
-        
+        // Позволяет определить являетсяли пространство слева от корабля игровым полем.
+        public static bool IsInGameplayAreaLeft (Transform objectTransform, Bounds objectBounds) {
+            var objectPos = objectTransform.position;
+
+            return objectPos.x - objectBounds.extents.x > leftBound;
+        }
+        // Позволяет определить являетсяли пространство справа от корабля игровым полем.
+        public static bool IsInGameplayAreaRight (Transform objectTransform, Bounds objectBounds) {
+            var objectPos = objectTransform.position;
+
+            return objectPos.x + objectBounds.extents.x < rightBound;
+        }
     }
 }
--- Assets/Scripts/Gameplay/ShipControllers/CustomControllers/PlayerShipController.cs
+++ Assets/Scripts/Gameplay/ShipControllers/CustomControllers/PlayerShipController.cs
@@ -1,15 +1,27 @@
 ﻿using Gameplay.ShipSystems;
+using Gameplay.Helpers;
 using UnityEngine;
 
 namespace Gameplay.ShipControllers.CustomControllers
 {
     public class PlayerShipController : ShipController
     {
+        [SerializeField]
+        private SpriteRenderer _representation; // Добавлено поле для хранения ссылки на спрайт корабля.
+
+        // Метод изменён для ограничения передвижения корабля игрока (в соответствии с требованиями ТЗ).
         protected override void ProcessHandling(MovementSystem movementSystem)
         {
-            movementSystem.LateralMovement(Input.GetAxis("Horizontal") * Time.deltaTime);
-        }
+            var axis = Input.GetAxis ("Horizontal");
 
+            if (axis > 0) {
+                if (GameAreaHelper.IsInGameplayAreaRight (transform, _representation.bounds))
+                    movementSystem.LateralMovement (axis * Time.deltaTime);
+            } else {
+                if (GameAreaHelper.IsInGameplayAreaLeft (transform, _representation.bounds))
+                    movementSystem.LateralMovement (axis * Time.deltaTime);
+            }
+        }
         protected override void ProcessFire(WeaponSystem fireSystem)
         {
             if (Input.GetKey(KeyCode.Space))
