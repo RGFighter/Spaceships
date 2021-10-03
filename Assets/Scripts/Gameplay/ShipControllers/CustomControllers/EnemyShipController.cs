19a3b7bc67369415a40e6b2e581825fef8f24a7b
RGFighter <sc3ragefighter@gmail.com>
2021-10-03 06:40:13 +0300

    Добавлено 3 типа новых кораблей.
    1. Добавлены корабли стреляющие ракетами, плазмой и энергией.
    2. Корабли стреляющие плазмой и энергией двигаются из стороны в сторону.
    3. У кораблей разное количество прочности и разное количество очков получаемых за их уничтожение.
    4. Добавлено 7 мест появления новых кораблей.
    5. Изменён класс EnemyShipController
    6. Подкорректировано энергетическое оружие.

--- Assets/Scripts/Gameplay/ShipControllers/CustomControllers/EnemyShipController.cs
+++ Assets/Scripts/Gameplay/ShipControllers/CustomControllers/EnemyShipController.cs
@@ -1,22 +1,38 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using Gameplay.ShipControllers;
 using Gameplay.ShipSystems;
 using UnityEngine;
 
+// Класс изменён.
 public class EnemyShipController : ShipController
 {
-
     [SerializeField]
     private Vector2 _fireDelay;
-
+    [SerializeField]
+    private float _oneDirectionMoveTime; // Добавлено поле для назначения времени движения корабля в одном направлении.
+    private float _lateralDirection = -1; // Добавлено поле для переключения направления бокового движения.
     private bool _fire = true;
-    
+
+	private void Awake () {
+        // Запускаем постоянную смену направления бокового движения, если это необходимо.
+        if (_oneDirectionMoveTime != 0)
+            StartCoroutine (LateralDirectionDelay (_oneDirectionMoveTime));
+    }
+    // Добавлен метод для ожидания переключения направления бокового движения.
+    private IEnumerator LateralDirectionDelay (float delay) {
+        while (true) {
+            yield return new WaitForSeconds (delay);
+            _lateralDirection *= -1;
+        }
+    }
     protected override void ProcessHandling(MovementSystem movementSystem)
     {
         movementSystem.LongitudinalMovement(Time.deltaTime);
-    }
 
+        // Боковое движение корабля.
+        if (_oneDirectionMoveTime != 0)
+            movementSystem.LateralMovement (_lateralDirection * Time.deltaTime);
+    }
     protected override void ProcessFire(WeaponSystem fireSystem)
     {
         if (!_fire)
@@ -25,8 +41,6 @@ public class EnemyShipController : ShipController
         fireSystem.TriggerFire();
         StartCoroutine(FireDelay(Random.Range(_fireDelay.x, _fireDelay.y)));
     }
-
-
     private IEnumerator FireDelay(float delay)
     {
         _fire = false;
